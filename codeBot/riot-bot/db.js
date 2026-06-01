00e0123a51f319d52708a635b5a467e886cc2408
obamadidao <103905682+obamadidao@users.noreply.github.com>
2026-06-01 21:23:02 +0700

    Update db.js

--- codeBot/riot-bot/db.js
+++ codeBot/riot-bot/db.js
@@ -1,9 +1,7 @@
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
-const db = new sqlite3.Database(
-  path.join(__dirname, "database.db")
-);
+const db = new sqlite3.Database("/data/database.db");
 
 db.run("PRAGMA journal_mode = WAL;");
 
